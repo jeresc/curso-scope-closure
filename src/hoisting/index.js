05b584c3fe9a8dcf7962357e4dc538dc294e36ba
jeresc <jeremiastomassrc@gmail.com>
2023-05-19 10:31:24 -0300

    JS Hoisting & Debugging

---
+++ src/hoisting/index.js
@@ -0,0 +1,11 @@
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+
+nameofDog();
+
+function nameofDog() {
+  console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito';
